47bf3e93a0456784b609f594baca8745b8995e56
natancohen <fl770fl@gmail.com>
2025-09-04 13:31:15 +0300

    remove log

--- src/components/Form/MiddleColumn.tsx
+++ src/components/Form/MiddleColumn.tsx
@@ -144,7 +144,7 @@ export default function MiddleColumn({
         {errors.damageType && <span className={styles.error}>{errors.damageType.message}</span>}
       </div>
 
-      {/* המלצות ראשוניות */}
+
       <div className={styles.fieldBox} style={{ minHeight: '120px' }}>
         <label className={styles.label}>המלצות ראשוניות</label>
         <textarea
